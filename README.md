a737545eeafeb99d1d02387649526076e82b7d4a
villhame <109059898+villhame@users.noreply.github.com>
2023-03-12 15:55:50 +1100

    feat: created console based RPS game

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Rock-paper-scissors
 Game of rock paper scissors
+12/03/23:
+Going to make a basic console version of this game. then later I will build some graphics on top;
+1. made a getComputerChoice() function to randomize paper scissors rock. using getRandomInt(max) which is a helper function found online
+2. made playRound( Player, Computer) function that plays one round of the game and returns a win, lose or draw message
+3. made a Game() function that plays 5 rounds of the game and if you win, draw or lose 3 or more games that is the outcome respectively
---
+++ script.js
@@ -0,0 +1,94 @@
+console.log("Hello World");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function getComputerChoice() {
+   let x = getRandomInt(3)
+   let arr = ['paper', 'scissors', 'rock'];
+   console.log('Computer: ' + arr[x]);
+   return arr[x];
+}
+
+function playRound( Player, Computer) {
+    Player = Player.toLowerCase();
+    console.log(Player);
+    //draw conditions
+    if( Player === 'rock' && Computer === 'rock') {
+        return 'It is a draw: Computer had rock as well.';
+    }
+    else if( Player === 'paper' && Computer === 'paper') {
+        return 'It is a draw: Computer had paper as well.';
+    }
+    else if( Player === 'scissors' && Computer === 'scissors') {
+        return 'It is a draw: Computer had scissors as well.';
+    }
+    // win conditions
+    else if( Player === 'rock' && Computer === 'scissors') {
+        return 'You win: Computer had scissors!';
+    }
+    else if( Player === 'paper' && Computer === 'rock') {
+        return 'You win: Computer had rock!';
+    }
+    else if( Player === 'scissors' && Computer === 'paper') {
+        return 'You win: Computer had paper!';
+    }
+    // lose conditions
+    else if( Player === 'rock' && Computer === 'paper') {
+        return 'You lose: Computer had paper!';
+    }
+    else if( Player === 'paper' && Computer === 'scissors') {
+        return 'You lose: Computer had scissors!';
+    }
+    else if( Player === 'scissors' && Computer === 'rock') {
+        return 'You lose: Computer had rock!';
+    }
+    else{
+        return 'invalid input'
+    }
+ }
+
+ function Game() {
+    let wCount = 0;
+    let lCount = 0;
+    let dCount = 0;
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound(prompt(), getComputerChoice());
+        console.log(roundResult);
+        //draw counts
+        if(roundResult === 'It is a draw: Computer had rock as well.' || roundResult === 'It is a draw: Computer had paper as well.' || roundResult === 'It is a draw: Computer had scissors as well.' ){
+            dCount += 1;
+            console.log('You draw this round');
+        }
+        //win counts
+        else if(roundResult === 'You win: Computer had scissors!' || roundResult === 'You win: Computer had rock!' || roundResult=== 'You win: Computer had paper!' ){
+            wCount += 1;
+            console.log('You win this round');
+        }
+        //lose counts
+        else if(roundResult === 'You lose: Computer had paper!' || roundResult === 'You lose: Computer had scissors!' || roundResult === 'You lose: Computer had rock!' ){
+            lCount += 1;
+            console.log('You lose this round');
+        }
+        else{
+            console.log('what happened');
+        }
+     }
+    if( wCount >= 3){
+        return 'You win!'
+    }
+    if( lCount >= 3){
+        return 'You lose!'
+    }
+    if( dCount >= 3){
+        return 'You draw!'
+    }
+ }
+
+ console.log(Game());
+
+// const input = prompt();
+
+
+
