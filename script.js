7d987cee8b6f224d34edef759bb5b7e2550d9682
villhame <109059898+villhame@users.noreply.github.com>
2023-09-22 17:36:25 +1000

    Basic functioning game is now working.
    Todo: add score counter, maybe a reset button...

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-
+//👊 ✋ ✌️
 console.log("Hello World");
 
+//Win/ lose/ draw/ results
+let wCount = 0;
+let lCount = 0;
+let dCount = 0;
+let huhCount = 0;
+
 const playText = document.querySelector('.playText')
-const btn = document.querySelector('.card')
+//const btn = document.querySelector('.card')
 //playText.textContent = "I changed this"
 
 function getRandomInt(max) {
@@ -21,93 +27,76 @@ function playRound( Player, Computer) {
     console.log(Player);
     //draw conditions
     if( Player === 'rock' && Computer === 'rock') {
-
-        return 'It is a draw: Computer had rock as well.';
+        dCount += 1;
+        return 'Draw: 🤖 had 👊 as well.' 
     }
     else if( Player === 'paper' && Computer === 'paper') {
-        return 'It is a draw: Computer had paper as well.';
+        dCount += 1;
+        return 'Draw: 🤖 had ✋ as well.'  
     }
     else if( Player === 'scissors' && Computer === 'scissors') {
-        return 'It is a draw: Computer had scissors as well.';
+        dCount += 1;
+        return 'Draw: 🤖 had ✌️ as well.' 
     }
     // win conditions
     else if( Player === 'rock' && Computer === 'scissors') {
-        return 'You win: Computer had scissors!';
+        wCount += 1;
+        return 'You win! 🤖 had ✌️' 
     }
     else if( Player === 'paper' && Computer === 'rock') {
-        return 'You win: Computer had rock!';
+        wCount += 1;
+        return 'You win! 🤖 had 👊' 
     }
     else if( Player === 'scissors' && Computer === 'paper') {
-        return 'You win: Computer had paper!';
+        wCount += 1;
+        return 'You win! 🤖 had ✋' 
     }
     // lose conditions
     else if( Player === 'rock' && Computer === 'paper') {
-        return 'You lose: Computer had paper!';
+        lCount += 1;
+        return 'You lose! 🤖 had ✋' 
     }
     else if( Player === 'paper' && Computer === 'scissors') {
-        return 'You lose: Computer had scissors!';
+        lCount += 1;
+        return 'You lose! 🤖 had ✌️' 
     }
     else if( Player === 'scissors' && Computer === 'rock') {
-        return 'You lose: Computer had rock!';
-    }
-    else{
-        return 'invalid input'
-    }
- }
-
- function results(roundResult){
-    let wCount = 0;
-    let lCount = 0;
-    let dCount = 0;
-    let huhCount = 0;
-
-    if(roundResult === 'It is a draw: Computer had rock as well.' || roundResult === 'It is a draw: Computer had paper as well.' || roundResult === 'It is a draw: Computer had scissors as well.' ){
-        dCount += 1;
-        console.log('You draw this round');
-    }
-    //win counts
-    else if(roundResult === 'You win: Computer had scissors!' || roundResult === 'You win: Computer had rock!' || roundResult=== 'You win: Computer had paper!' ){
-        wCount += 1;
-        console.log('You win this round');
-    }
-    //lose counts
-    else if(roundResult === 'You lose: Computer had paper!' || roundResult === 'You lose: Computer had scissors!' || roundResult === 'You lose: Computer had rock!' ){
         lCount += 1;
-        console.log('You lose this round');
+        return 'You lose! 🤖 had 👊' 
     }
     else{
         huhCount += 1;
-        console.log('what happened');
+        return 'invalid input'
     }
- 
+}
+
+function gameOver() {
+    
+    //win conditions
     if( wCount >= 3){
         console.log('You win!');
-        playText.textContent = "You win!";
-        return 'You win!';
+        return "You won against 🤖!";;
     }
+
     else if( lCount >= 3){
         console.log('You lose!');
-        playText.textContent = "You lost!";
-        return 'You lose!';
+        return "You lost to 🤖!";
     }
+
     else if( dCount >= 3){
         console.log('You Draw!');
-        playText.textContent = "You Draw!";
-        return 'You draw!';
+        return 'You Draw!';
     }
-    else if( huhCount >= 3){
-        console.log('You do not understand how to play huh');
-        playText.textContent = "What happend";
-        return 'You do not understand how to play huh';
+    else{
+        return "yeet";
     }
- }
+}
 
-function playOption(){
-    //Win/ lose/ draw/ results
 
+function playOption(){    
     let roundResult;
 
-    //what happens when yu click Rock
+    //what happens when you click Rock
     const Rock = document.querySelector('#Rock');
     Rock.addEventListener('click', () => {
         console.log('rock');
@@ -115,34 +104,54 @@ function playOption(){
         roundResult = playRound('rock', getComputerChoice());
         playText.textContent = roundResult;
         //think about adding delay
-        //playText.textContent = results(roundResult);
+        console.log("Current wCount/dCount/lCount: " + wCount + "/ " + dCount + "/ " + lCount );
+        //win conditions
+            if(wCount >= 3 || lCount >= 3 || dCount >= 3){
+                playText.textContent = gameOver();
+            }
         return 'rock';
       });
 
-    //what happens when yu click Paper
+    //what happens when you click Paper
     const Paper = document.querySelector('#Paper');
     Paper.addEventListener('click', () => {
         console.log('paper');
         playText.textContent = "You picked paper" 
         roundResult = playRound('paper', getComputerChoice());
         playText.textContent = roundResult;
-        //playText.textContent = results(roundResult);
+        console.log("Current wCount/dCount/lCount: " + wCount + "/ " + dCount + "/ " + lCount );
+        //playText.textContent = gameOver();
+               //win conditions
+            if(wCount >= 3 || lCount >= 3 || dCount >= 3){
+                playText.textContent = gameOver();
+            }
         return 'paper';
       });
-    //what happens when yu click Scissors
+
+    //what happens when you click Scissors
     const Scissors = document.querySelector('#Scissors');
     Scissors.addEventListener('click', () => { //function (e) {
         console.log('scissors');
         playText.textContent = "You picked scissors" 
         roundResult = playRound('scissors', getComputerChoice());
         playText.textContent = roundResult;
-        //playText.textContent = results(roundResult);
+        console.log("Current wCount/dCount/lCount: " + wCount + "/ " + dCount + "/ " + lCount );
+               //win conditions
+            if(wCount >= 3 || lCount >= 3 || dCount >= 3){
+                playText.textContent = gameOver();
+            }
+        //playText.textContent = gameOver();
         return 'scissors';
       });
 
+
+      
 }
 
 playOption();
+
+
+
+
+
 playText.append()
-
-
