c98d994b479ee65c7aa1b76f14686772684355e0
villhame <109059898+villhame@users.noreply.github.com>
2023-09-22 15:58:13 +1000

    Integrated my console code into the UI

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+
 console.log("Hello World");
+
+const playText = document.querySelector('.playText')
+const btn = document.querySelector('.card')
+//playText.textContent = "I changed this"
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -12,10 +17,11 @@ function getComputerChoice() {
 }
 
 function playRound( Player, Computer) {
-    Player = Player.toLowerCase();
+    //Player = Player.toLowerCase();
     console.log(Player);
     //draw conditions
     if( Player === 'rock' && Computer === 'rock') {
+
         return 'It is a draw: Computer had rock as well.';
     }
     else if( Player === 'paper' && Computer === 'paper') {
@@ -49,46 +55,94 @@ function playRound( Player, Computer) {
     }
  }
 
- function Game() {
+ function results(roundResult){
     let wCount = 0;
     let lCount = 0;
     let dCount = 0;
-    //for (let i = 0; i < 5; i++) {
-        let roundResult = playRound(prompt(), getComputerChoice());
-        console.log(roundResult);
-        //draw counts
-        if(roundResult === 'It is a draw: Computer had rock as well.' || roundResult === 'It is a draw: Computer had paper as well.' || roundResult === 'It is a draw: Computer had scissors as well.' ){
-            dCount += 1;
-            console.log('You draw this round');
-        }
-        //win counts
-        else if(roundResult === 'You win: Computer had scissors!' || roundResult === 'You win: Computer had rock!' || roundResult=== 'You win: Computer had paper!' ){
-            wCount += 1;
-            console.log('You win this round');
-        }
-        //lose counts
-        else if(roundResult === 'You lose: Computer had paper!' || roundResult === 'You lose: Computer had scissors!' || roundResult === 'You lose: Computer had rock!' ){
-            lCount += 1;
-            console.log('You lose this round');
-        }
-        else{
-            console.log('what happened');
-        }
-     }
+    let huhCount = 0;
+
+    if(roundResult === 'It is a draw: Computer had rock as well.' || roundResult === 'It is a draw: Computer had paper as well.' || roundResult === 'It is a draw: Computer had scissors as well.' ){
+        dCount += 1;
+        console.log('You draw this round');
+    }
+    //win counts
+    else if(roundResult === 'You win: Computer had scissors!' || roundResult === 'You win: Computer had rock!' || roundResult=== 'You win: Computer had paper!' ){
+        wCount += 1;
+        console.log('You win this round');
+    }
+    //lose counts
+    else if(roundResult === 'You lose: Computer had paper!' || roundResult === 'You lose: Computer had scissors!' || roundResult === 'You lose: Computer had rock!' ){
+        lCount += 1;
+        console.log('You lose this round');
+    }
+    else{
+        huhCount += 1;
+        console.log('what happened');
+    }
+ 
     if( wCount >= 3){
-        return 'You win!'
+        console.log('You win!');
+        playText.textContent = "You win!";
+        return 'You win!';
     }
-    if( lCount >= 3){
-        return 'You lose!'
+    else if( lCount >= 3){
+        console.log('You lose!');
+        playText.textContent = "You lost!";
+        return 'You lose!';
     }
-    if( dCount >= 3){
-        return 'You draw!'
+    else if( dCount >= 3){
+        console.log('You Draw!');
+        playText.textContent = "You Draw!";
+        return 'You draw!';
     }
- //}
+    else if( huhCount >= 3){
+        console.log('You do not understand how to play huh');
+        playText.textContent = "What happend";
+        return 'You do not understand how to play huh';
+    }
+ }
 
- console.log(Game());
+function playOption(){
+    //Win/ lose/ draw/ results
 
-// const input = prompt();
+    let roundResult;
 
+    //what happens when yu click Rock
+    const Rock = document.querySelector('#Rock');
+    Rock.addEventListener('click', () => {
+        console.log('rock');
+        playText.textContent = "You picked rock" 
+        roundResult = playRound('rock', getComputerChoice());
+        playText.textContent = roundResult;
+        //think about adding delay
+        //playText.textContent = results(roundResult);
+        return 'rock';
+      });
+
+    //what happens when yu click Paper
+    const Paper = document.querySelector('#Paper');
+    Paper.addEventListener('click', () => {
+        console.log('paper');
+        playText.textContent = "You picked paper" 
+        roundResult = playRound('paper', getComputerChoice());
+        playText.textContent = roundResult;
+        //playText.textContent = results(roundResult);
+        return 'paper';
+      });
+    //what happens when yu click Scissors
+    const Scissors = document.querySelector('#Scissors');
+    Scissors.addEventListener('click', () => { //function (e) {
+        console.log('scissors');
+        playText.textContent = "You picked scissors" 
+        roundResult = playRound('scissors', getComputerChoice());
+        playText.textContent = roundResult;
+        //playText.textContent = results(roundResult);
+        return 'scissors';
+      });
+
+}
+
+playOption();
+playText.append()
 
 
